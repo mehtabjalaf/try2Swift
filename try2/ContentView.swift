c564ee4f7ee1575fd5c159dbf657c27619fc81ea
mehtab <msj420@my.yorku.ca>
2024-03-23 18:15:31 -0400

    Initial Commit

---
+++ try2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  try2
+//
+//  Created by Mehtab Jalaf on 2024-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ try2/try2App.swift
@@ -0,0 +1,17 @@
+//
+//  try2App.swift
+//  try2
+//
+//  Created by Mehtab Jalaf on 2024-03-23.
+//
+
+import SwiftUI
+
+@main
+struct try2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
